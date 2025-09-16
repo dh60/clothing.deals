cf6f3e13f9402788e70ce65e17ba1bdaff5550d0
dh60 <d.h@unb.ca>
2025-09-16 18:27:44 -0300

    Removed unnecessary dependencies

--- app.py
+++ app.py
@@ -1,38 +1,29 @@
 import asyncio
 import orjson
-import os
 import aiofiles
-import yaml
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse, Response
-from fastapi.templating import Jinja2Templates
-from scraper import SsenseScraper, Product
+from fastapi import FastAPI
+from fastapi.responses import FileResponse, Response
+from ssense import SsenseScraper, Product
 
 app = FastAPI()
-templates = Jinja2Templates(directory=".")
 CACHE_FILE = 'products_cache.json'
 all_products = []
 
 async def run_scrape():
     global all_products
-    if os.path.exists(CACHE_FILE):
-        async with aiofiles.open(CACHE_FILE, 'rb') as f:
-            data = await f.read()
-        all_products = orjson.loads(data)
-    else:
-        scraped_products = await SsenseScraper().scrape_all()
-        all_products = [p.__dict__ for p in scraped_products]
-        async with aiofiles.open(CACHE_FILE, 'wb') as f:
-            await f.write(orjson.dumps(all_products))
-          
-@app.get("/", response_class=HTMLResponse)
-async def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
-  
+    scraped_products = await SsenseScraper().scrape_all()
+    all_products = [p.__dict__ for p in scraped_products]
+    async with aiofiles.open(CACHE_FILE, 'wb') as f:
+        await f.write(orjson.dumps(all_products))
+
+@app.get("/")
+async def index():
+    return FileResponse("index.html")
+
 @app.get("/data")
 async def get_data():
     return Response(content=orjson.dumps(all_products), media_type="application/json")
-  
+
 if __name__ == '__main__':
     import uvicorn
     asyncio.run(run_scrape())
