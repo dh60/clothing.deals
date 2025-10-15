3e3a873ee5b8409e85f4d02738a4e629a7c4ad8e
dh60 <d.h@unb.ca>
2025-10-15 19:10:52 -0300

    Reduced error spam.

--- ssense.py
+++ ssense.py
@@ -33,8 +33,8 @@ async def fetch(url, page, pool, lock, nocaptcha):
                             nocaptcha.set()
                     continue
                 tqdm.write(f"{status} for {url}, Attempt {attempt + 1}/{RETRIES}")
-            except Exception as e:
-                tqdm.write(f"Error for {url}, Attempt {attempt + 1}/{RETRIES}: {e}")
+            except Exception:
+                tqdm.write(f"Error for {url}, Attempt {attempt + 1}/{RETRIES}")
         await asyncio.sleep(DELAY)
     tqdm.write(f"Skipping {url} after {RETRIES} retries.")
     return None
