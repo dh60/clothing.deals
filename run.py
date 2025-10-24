7d11567cdaf4d3e0b943be1404355e931f1a8bee
dh60 <d.h@unb.ca>
2025-10-24 10:46:23 -0300

    Use localhost, open browser tab, remove main.

--- run.py
+++ run.py
@@ -1,13 +1,11 @@
 import uvicorn
+import webbrowser
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
-
 app = FastAPI()
-
 @app.get("/")
 async def serve_index():
     return FileResponse("index.html")
-
 @app.get("/categories.json.br")
 async def serve_category_data():
     return FileResponse(
@@ -15,7 +13,6 @@ async def serve_category_data():
         media_type="application/json",
         headers={"Content-Encoding": "br"}
     )
-
 @app.get("/products.json.br")
 async def serve_product_data():
     return FileResponse(
@@ -23,8 +20,5 @@ async def serve_product_data():
         media_type="application/json",
         headers={"Content-Encoding": "br"}
     )
-
-if __name__ == '__main__':
-    print("--- Starting the web server at http://127.0.0.1:8000 ---")
-    print("Open your web browser to that address to view the app.")
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+webbrowser.open('http://localhost:8000')
+uvicorn.run(app, host='localhost', port=8000)
